f182ce0f41199baf418b3efbdf3a868b0e371929
liko <likcoah@gmail.com>
2025-12-17 19:22:53 +0500

    Start: starting point of the project

---
+++ src/game_loop.cpp
@@ -0,0 +1,7 @@
+#include "game_loop.hpp"
+#include <iostream>
+
+void GameLoop::helloWorld() {
+	std::cout << "Hello world" << std::endl;
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "game_loop.hpp"
+
+int main() {
+	GameLoop loop;
+	loop.helloWorld();
+	return 0;
+}
